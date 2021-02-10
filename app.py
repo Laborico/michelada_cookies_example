442db709e0fa58c02d1d80bbeb4bb6feba625d24
Laborico <david97132@hotmail.com>
2021-02-10 12:13:40 -0600

    Variable pass on URL

--- app.py
+++ app.py
@@ -6,45 +6,43 @@ from flask import Flask, render_template, make_response, request, redirect, sess
 
 
 app = Flask(__name__)
-app.permanent_session_lifetime = datetime.timedelta(days=365)
-app.secret_key = "hD4GbkeacT3KXuwZ"
+app.secret_key = 'Z9GP$75[q-sf9Fi=.!>r$X"sYlTzZ"vZP8_#b<Bt~nFG?%klFe=|l?p"k|2$;9*'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   """
-   Home
-   :return:
-   :rtype:
-   """
    if request.method == 'POST':
       email = request.form.get('email')
       password = request.form.get('password')
       if email != '' and password !='':
-         session['email']=email
-         session['password']=password
-         return redirect(f'/backoffice')
+         return redirect(f'/backoffice?email={email}&password={password}')
 
    return render_template("login.html"), 200
 
 
 @app.route('/backoffice')
 def backoffice(methods=['GET']):
-   if 'email' in session:
-      return render_template("backoffice.html",info={'email':session['email'],'password':session['password']}), 200
+
+   email=request.args.get('email', type= str)
+   password=request.args.get('password', type= str)
+
+   if len(email)>0 and len(password)>0:
+      Info_de_user={
+         'email':email,
+         'password':password
+      }
+      return render_template("backoffice.html",Info_de_user=Info_de_user), 200
    else:
       flash('Primero debes de iniciar sesion')
       return redirect(f'/login')
 
 @app.route('/logout')
-def logout():
-    session.pop('email')         
-    session.pop('password')         
+def logout():  
     return redirect('/login')
 
 @app.route('/')
 def home():
-    return redirect(f'/backoffice')
+    return redirect(f'/login')
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=8080)
